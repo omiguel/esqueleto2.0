e9c601320b879d6499f6824922f74e3f656f8338
gustavo <gurbrillinger@gmail.com>
2016-05-26 14:19:09 -0300

    agora recebo nome do atributo e tipo atributo

--- public/scripts/controllers/entidadesController.js
+++ public/scripts/controllers/entidadesController.js
@@ -5,21 +5,32 @@ app.controller("entidadesController",['$scope', function ($scope) {
     var me = this;
 
     $scope.entidades = {};
-    $scope.entidadeSelecionado = {}
+    $scope.entidadeSelecionada = {}
 
     var listeners = {};
 
+    /*
+    * criado por: Gustavo
+    * joga a entidade desejada dentro da variavel $scope.entidadeSelecionada
+    */
     $scope.selecionaEntidade = function(entidade){
-        $scope.entidadeSelecionado = entidade;
+        $scope.entidadeSelecionada = angular.copy(entidade);
     };
 
+    /*
+    * criado por: Osvaldo
+    */
     var ready = function () {
         var msg = new Mensagem(me, 'getallmodels', {}, 'entidades');
         SIOM.emitirServer(msg);
     };
 
+    /*
+    * criado por: Osvaldo
+    */
     var retallmodels = function (msg) {
         $scope.entidades = msg.getDado();
+        console.log("ent", msg.getDado());
         $scope.$apply();
     };
 
